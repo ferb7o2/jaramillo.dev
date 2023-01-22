3042360b552a464f25db664701b9acc200007e8a
JoseJaramillo04 <jose.jaramillo04@utrgv.edu>
2023-01-22 01:47:29 -0600

    finished set up of nextjs13.1

---
+++ .next/server/flight-server-css-manifest.js
@@ -0,0 +1,16 @@
+self.__RSC_CSS_MANIFEST={
+  "__entry_css_mods__": {
+    "/Users/ferb7o2/personalCode/jaramillo.dev/src/app/page": [
+      "/Users/ferb7o2/personalCode/jaramillo.dev/src/app/page.module.css",
+      "/Users/ferb7o2/personalCode/jaramillo.dev/node_modules/@next/font/google/target.css?{\"path\":\"src/app/page.tsx\",\"import\":\"Inter\",\"arguments\":[{\"subsets\":[\"latin\"]}],\"variableName\":\"inter\"}",
+      "/Users/ferb7o2/personalCode/jaramillo.dev/src/app/globals.css"
+    ]
+  },
+  "/Users/ferb7o2/personalCode/jaramillo.dev/src/app/page.tsx": [
+    "/Users/ferb7o2/personalCode/jaramillo.dev/src/app/page.module.css",
+    "/Users/ferb7o2/personalCode/jaramillo.dev/node_modules/@next/font/google/target.css?{\"path\":\"src/app/page.tsx\",\"import\":\"Inter\",\"arguments\":[{\"subsets\":[\"latin\"]}],\"variableName\":\"inter\"}"
+  ],
+  "/Users/ferb7o2/personalCode/jaramillo.dev/src/app/layout.tsx": [
+    "/Users/ferb7o2/personalCode/jaramillo.dev/src/app/globals.css"
+  ]
+}
